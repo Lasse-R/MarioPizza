acdf07fcc73b06b332b6bc6a50c210f5ee116031
victor-dab21b <vict702k@stud.kea.dk>
2021-10-13 11:20:09 +0200

    Pizzabar v4

--- src/Bestilling.java
+++ src/Bestilling.java
@@ -1,2 +1,15 @@
 public class Bestilling {
+    static int statiskBestillingCounter = 0;
+    private int counterForObject;
+    public Bestilling(){
+        statiskBestillingCounter++;
+        counterForObject = statiskBestillingCounter;
+    }
+
+
+
+    public int giveBackCounter(){
+        return counterForObject;
+    }
+
 }
