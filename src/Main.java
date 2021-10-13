fa0afcec1e6d95f28bcac7d306eb1768ea3f6f19
Troels <troe1912@stud.kea.dk>
2021-10-13 12:30:57 +0200

    Oprettet prinMenu()

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         Menukort menukort = new Menukort();
-    Pizza pizza1 = new Pizza(1);
-
-        System.out.println(pizza1.getName());
-        menukort.menuCard.add(pizza1);
-        menukort.menuCard.add(pizza1);
-        menukort.menuCard.add(pizza1);
-
-        System.out.println(menukort.menuCard);
+        menukort.menuCreate();
+        menukort.printMenu();
 
     }
 }
