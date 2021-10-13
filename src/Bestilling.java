dd024a195ba9f663461f9cab23ca740114f80553
victor-dab21b <vict702k@stud.kea.dk>
2021-10-13 13:09:17 +0200

    Pizzabar ekstra på Bestilling og BestillingsListe

--- src/Bestilling.java
+++ src/Bestilling.java
@@ -7,7 +7,10 @@ public class Bestilling {
     public Bestilling(){
         statiskBestillingCounter++;
         counterForObject = statiskBestillingCounter;
-    }
+
+        }
+
+
 
 
     public int giveBackCounter(){
