22a0406ca7000ef6e57d0b550a4ddfff4653b342
victor-dab21b <vict702k@stud.kea.dk>
2021-10-13 11:02:22 +0200

    Pizzabar v2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+    Pizza pizza1 = new Pizza(1);
+        System.out.println(pizza1.getName());
 
     }
 }
--- src/Pizza.java
+++ src/Pizza.java
@@ -1,2 +1,42 @@
 public class Pizza {
+    private String name;
+    private int price;
+    private String standardToppings;
+    private String ekstraToppings;
+
+    public Pizza(int whichPizza){
+        thirtyPizza(whichPizza);
+    }
+    public Pizza(int whichPizza, String ekstraTop) {
+        thirtyPizza(whichPizza);
+    }
+
+
+
+
+
+
+
+
+        public void thirtyPizza(int whichPizza){
+        switch (whichPizza){
+            case 1:
+                this.name = "Margarita";
+                this.price = 50;
+                this.standardToppings = "tomat, ost";
+                break;
+            case 2:
+                break;
+
+        }
+
+
+
+
+    }
+    public String getName(){
+        return this.name;
+    }
+
+
 }
