be75e050c03ab2588d74f0f782674b643b7f8b1e
victor-dab21b <vict702k@stud.kea.dk>
2021-10-15 11:57:30 +0200

    Singleton

--- src/Arkiv.java
+++ src/Arkiv.java
@@ -1,4 +1,24 @@
+import java.util.ArrayList;
+
 public class Arkiv {
+    private ArrayList<Bestilling> allArchivedOrders = new ArrayList<>();
+    private static Arkiv INSTANCE;
+    private String info = "Initial info class";
+
+    private Arkiv() {
+    }
+
+    public static Arkiv getInstance() {
+        if(INSTANCE == null) {
+            INSTANCE = new Arkiv();
+        }
+
+        return INSTANCE;
+    }
+
+
+
+
 
 
 }
