66e90b4198717273f061a8d4c1a7f4d29e5049a5
victor-dab21b <vict702k@stud.kea.dk>
2021-10-15 12:08:59 +0200

    Singleton og pm2000

--- src/PM2000.java
+++ src/PM2000.java
@@ -1,35 +1,52 @@
 public class PM2000 {
-    public static void main(String[] args) {
 
-        Menukort menukort = new Menukort();
-        BestillingsListe bestillingsListe = new BestillingsListe();
-        Bestilling bestilling1 = new Bestilling();
-        //menukort.menuCreate();
-        //menukort.printMenu();
+        public void startProgram () {
+            Menukort menukort = new Menukort();
+            BestillingsListe bestillingsListe = new BestillingsListe();
+            Arkiv theArchive = Arkiv.getInstance();
 
-        bestilling1.addPizza(1);
-        //System.out.println(bestilling1.getPizzasInOrder());
+            boolean runningFlag = true;
+            while(runningFlag){
+                System.out.println("PizzaMachine2000 is starting");
 
-        Bestilling bestilling2 = new Bestilling();
-        bestilling2.addPizza(4);
-
-        bestillingsListe.activeOrders(bestilling1);
-        bestillingsListe.activeOrders(bestilling2);
-
-        int bestillingsNummer = 3;
-
-
-        for(int i = 0; i < bestillingsListe.allOrders.size(); i++){
-            if(bestillingsListe.allOrders.get(i).giveBackCounter() == bestillingsNummer){
-                System.out.println("fandt denne");
-                System.out.println(bestillingsListe.allOrders.get(i));
-
-                bestillingsListe.allOrders.remove(i);
             }
-        }
+            /*
+            //System.out.println(bestilling1.getPizzasInOrder());
+
+            Bestilling bestilling2 = new Bestilling();
+            bestilling2.addPizza(4);
+
+            bestillingsListe.activeOrders(bestilling1);
+            bestillingsListe.activeOrders(bestilling2);
+
+            int bestillingsNummer = 3;
 
 
 
+            for (int i = 0; i < bestillingsListe.allOrders.size(); i++) {
+                if (bestillingsListe.allOrders.get(i).giveBackCounter() == bestillingsNummer) {
+                    System.out.println("fandt denne");
+                    System.out.println(bestillingsListe.allOrders.get(i));
 
+                    bestillingsListe.allOrders.remove(i);
+                }
+
+             */
+            }
+
+
+    public static void main(String[] args) {
+        PM2000 pmrunTime = new PM2000();
+        pmrunTime.startProgram();
     }
-}
+    }
+
+
+
+
+
+
+
+
+
+
